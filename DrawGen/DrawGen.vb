ee3ddb7f3dd394d716feb1d14b462de321047e8e
Edward Jackson <53570068+BACEjackson@users.noreply.github.com>
2021-04-23 09:41:25 -0400

    Added code to put Date and version number

--- DrawGen/DrawGen.vb
+++ DrawGen/DrawGen.vb
@@ -2766,7 +2766,7 @@ Err_getdims:
                                     If ConfigVersion = "Nothing" Then
                                         varAttributes(j).TextString = Now()  
                                     Else
-                                       varAttributes(j).TextString = ConfigVersion
+                                       varAttributes(j).TextString = Now() & "  " & ConfigVersion
                                     End If
                                 End if
                                 If varAttributes(j).TagString = "DESC" Then varAttributes(j).TextString = strCustData
